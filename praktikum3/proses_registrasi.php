d0624c0a4e69295f84d394817bd13066c2e7828f
ilyasbdlzz <ilyaskenway@gmail.com>
2024-05-17 13:32:05 +0700

    Praktikum 3 - PHP Awal (Proses)

---
+++ praktikum3/proses_registrasi.php
@@ -0,0 +1,20 @@
+<?php
+//Array Numeric
+$domisili =["Jakarta", "Bogor","Depok","Tangerang","Bekasi","Jupiter","Bandung"];
+
+//Array Assosiative
+$program_studi = [
+    "SI" => "Sistem Informasi",
+    "TI" => "Teknik Informatika",
+    "BD" => "Bisnis Digital"
+];
+
+//Nilai setiap skill 
+$nilai_skill = array(
+    "HTML" => 10,
+    "CSS" => 10,
+    "JavaScript" => 20,
+    "PHP" => 30,
+    "Python" => 30
+);
+?>
